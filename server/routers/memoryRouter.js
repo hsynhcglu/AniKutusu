76ac9686dd2ea71ddb44eff3fd527201fda60b89
huseyin <huseyinhacioglu55@icloud.com>
2022-07-27 17:05:53 +0300

    post fixed

--- server/routers/memoryRouter.js
+++ server/routers/memoryRouter.js
@@ -12,7 +12,6 @@ router.get('/', async (req,res) => {
 
         res.status(200).json(memories)
 
-        res.json(memories)
     }catch(error) {
         res.status(400).json({message: 'gg'})
     }
