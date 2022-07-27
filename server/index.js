2b8810f9679e180a1e36fce49e0463930e4ede50
huseyin <huseyinhacioglu55@icloud.com>
2022-07-27 16:41:31 +0300

    anikutusu created

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import memoryRouter from './routers/memoryRouter.js'
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json({ limit: '20mb' }))
+
+app.use('/memories', memoryRouter)
+
+app.listen(process.env.PORT, () => {
+    mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log('connected to db'))
+    .catch((err) => console.log(err))
+})
---
+++ server/routers/memoryRouter.js
@@ -0,0 +1,67 @@
+import express, { application } from 'express'
+import mongoose from 'mongoose'
+import Memory from '../db/memoryModel.js'
+
+const router = express.Router()
+
+//Get all memories from db 
+
+router.get('/', async (req,res) => {
+    try{
+        const memories = await Memory.find()
+
+        res.status(200).json(memories)
+
+        res.json(memories)
+    }catch(error) {
+        res.status(400).json({message: 'gg'})
+    }
+})
+
+//Get single memory from db
+
+router.get('/:id', async(req,res) => {
+    try {
+        const {id} = req.params
+
+        if(!mongoose.Types.ObjectId.isValid(id))
+        res.status(404).json({message: 'Memory id is not valid'})
+
+        const memory = await Memory.findById(id)
+        if(!memory) return
+
+        res.status(200).json(memory)
+
+    }catch (error) {
+        res.status(404).json({message: 'memory not found'})
+    }
+})
+
+//Create a memory
+
+router.post('/', async (req, res) => {
+    try {
+      const memory = req.body
+  
+      const createdMemory = await Memory.create(memory)
+  
+      res.status(201).json(createdMemory)
+    } catch (error) {
+      console.log(error.message)
+      res.json({ message: 'Create memory failed' })
+    }
+  })
+
+//Update a memory
+
+router.put('/:id', async(req,res) => {
+    res.json({message: 'update a memory from db'})
+})
+
+//Delete a memory
+
+router.delete('/:id', async(req,res) => {
+    res.json({message: 'delete a memory from db'})
+})
+
+export default router;
