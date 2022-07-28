01cb83d51a917861e8b32158a86ed1dc6d4b4c3f
huseyin <huseyinhacioglu55@icloud.com>
2022-07-28 22:48:26 +0300

    some changes

--- client/src/reducers/MemoriesReducer.js
+++ client/src/reducers/MemoriesReducer.js
@@ -9,18 +9,14 @@ export default (memories = [], action) => {
   switch (action.type) {
     case FETCH_ALL:
       return action.payload
-
     case CREATE:
       return [...memories, action.payload]
-
     case UPDATE:
       return memories.map((memory) =>
         memory._id === action.payload._id ? action.payload : memory
       )
-
     case DELETE:
       return memories.filter((memory) => memory._id !== action.payload)
-
     default:
       return memories
   }
